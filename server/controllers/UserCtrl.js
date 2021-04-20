6fac8764d53c631c3768f01d291358cfdd1ac25c
billybep <billy.pesoth@gmail.com>
2021-04-20 20:57:44 +0700

    add user signup

--- server/controllers/UserCtrl.js
+++ server/controllers/UserCtrl.js
@@ -1,8 +1,21 @@
-
+const { User } = require('../models')
 class UserCtrl {
 
   static signup (req, res, next) {
-    console.log('signnnup')
+    const newUser = {
+      username: req.body.username,
+      email   : req.body.email,
+      password: req.body.password
+    }
+
+    User
+      .create({ ...newUser })
+      .then(data => res.status(201).json({ 
+        id        : data.id,
+        username  : data.username,
+        email     : data.email
+      }))
+      .catch(err => next(err))
   }
 
 }
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+
+const { encryptPassword } = require('../helpers/bcrypt')
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -14,12 +17,29 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    username: {
+      type    : DataTypes.STRING,
+      validate: { notEmpty: true }
+    },
+    email: {
+      type    : DataTypes.STRING,
+      validate: { isEmail : true }
+    },
+    password: {
+      type    : DataTypes.STRING,
+      validate: {
+        len   : {
+          args: [6],
+          msg : 'Your password must be at least 6 characters long. Please try another'
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
   });
+
+  User.beforeCreate(instance => instance.password = encryptPassword(instance.password))
+
   return User;
 };
